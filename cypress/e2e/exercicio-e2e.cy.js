06e75701798ad27ff21a75c45e3d7d0895a0dcce
Rodolfo <rodolfodutraneto2021@gmail.com>
2025-01-26 17:38:42 -0300

    faker

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,28 +1,22 @@
-import HomePage from '../../cypress/pages/HomePage';
-import CheckoutPage from  '../../cypress/pages/CheckoutPage';
-describe.only('Fluxo de Pedido na Loja EBAC', () => {
-  beforeEach(() => {
-    HomePage.visitar();
-  });
+/// <reference types="cypress" />
+import { faker } from "@faker-js/faker";
 
+describe('Funcionalidade Cadastro', () => {
+    beforeEach(() => {
+
+      cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+      
+    });
+      
   it('Deve realizar um pedido de ponta a ponta', () => {
-    cy.fixture('cliente').then((cliente) => {
-      // Adicionar 4 produtos ao carrinho
-      for (let i = 0; i < 4; i++) {
-        HomePage.buscarProdutoLista()
-        HomePage.addProdutoCarrinho()
-        HomePage.visitar('');
-      }
+    
+      })
 
       // Ir para o checkout e preencher os dados do cliente
-      CheckoutPage.irParaCheckout(cliente);
-      CheckoutPage.preencherCheckout(cliente);
+      
 
       // Finalizar a compra
-      CheckoutPage.finalizarCompra();
-
+      
       // Validar a confirmação da compra
-      cy.get('.woocommerce-thankyou-order-received').should('contain', 'Pedido finalizado com sucesso');
-    });
+      
   });
-});
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-
+import { faker } from "@faker-js/faker";
 describe('Deve fazer login com sucesso', () => {
 
     beforeEach(() => {
@@ -18,17 +18,17 @@ describe('Deve fazer login com sucesso', () => {
     });
 
     it('Deve exibir uma mensagem de erro ao inserir o usuário inválido', () => {
-        cy.get('#username').type('email.teste@teste.com')
+        cy.get('#username').type(faker.internet.userName())
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error').should('contain','Endereço de e-mail desconhecido.')
+        cy.get('.woocommerce-error').should('contain',' Se você não está certo de seu nome de usuário')
         cy.get('.woocommerce-error').should('exist')
 
     });
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
         cy.get('#username').type('email.teste@teste.com.br')
-        cy.get('#password').type('teste@000')
+        cy.get('#password').type(faker.internet.password())
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha')
         cy.get('.woocommerce-error').should('exist')
