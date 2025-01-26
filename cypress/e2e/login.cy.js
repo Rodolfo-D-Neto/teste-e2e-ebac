6955c7b0ad9605ab536acdde923716532fc5485a
Rodolfo <rodolfodutraneto2021@gmail.com>
2025-01-26 16:18:46 -0300

    Teste negativos e hooks

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,38 @@
 /// <reference types="cypress" />
 
 describe('Deve fazer login com sucesso', () => {
-    it('Deve fazer login com sucesso', () => {
+
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        
+    });
+ 
+    //afterEach(() => {
+    //cy.screenshot()
+    //});
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('email.teste@teste.com.br')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(3)'). should('contain', 'A partir do painel de controle de sua conta,')
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir o usuário inválido', () => {
+        cy.get('#username').type('email.teste@teste.com')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain','Endereço de e-mail desconhecido.')
+        cy.get('.woocommerce-error').should('exist')
+
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('email.teste@teste.com.br')
+        cy.get('#password').type('teste@000')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha')
+        cy.get('.woocommerce-error').should('exist')
+        
     });
 
 });
